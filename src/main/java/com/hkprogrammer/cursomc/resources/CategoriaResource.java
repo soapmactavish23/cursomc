7f680804640090dfb4110e695b8bdb63b48edfc5
HKprogram <ricknogueira1231@gmail.com>
2022-02-16 12:22:26 -0300

    Banco de dados H2 e criação automática da base de dados

--- src/main/java/com/hkprogrammer/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/hkprogrammer/cursomc/resources/CategoriaResource.java
@@ -15,12 +15,7 @@ public class CategoriaResource {
 
 	@GetMapping
 	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
 		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
 		
 		return lista;
 	}
