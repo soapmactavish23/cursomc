1ac6ab54a163324d0f8b799ef25828001395cfba
Henrick <ricknogueira1231@gmail.com>
2022-11-08 22:02:22 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/hkprogrammer/cursomc/resources/PedidoResource.java
@@ -0,0 +1,27 @@
+package com.hkprogrammer.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hkprogrammer.cursomc.domain.Pedido;
+import com.hkprogrammer.cursomc.services.PedidoService;
+
+@RestController
+@RequestMapping("/pedidos")
+public class PedidoResource {
+
+	@Autowired
+	private PedidoService service;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		System.out.println(id);
+		Pedido obj = service.buscar(id);
+		return ResponseEntity.ok(obj);
+	}
+	
+}
---
+++ src/main/java/com/hkprogrammer/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.hkprogrammer.cursomc.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hkprogrammer.cursomc.domain.Pedido;
+import com.hkprogrammer.cursomc.repositories.PedidoRepository;
+import com.hkprogrammer.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Pedido pedido = repo.findById(id).orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! " + id + ", Tipo: " + Pedido.class.getName()));
+		
+		return pedido;
+	}
+	
+}
