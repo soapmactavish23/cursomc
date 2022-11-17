67db136081384fa672d0be6821fa3fa01062ec8e
Henrick <ricknogueira1231@gmail.com>
2022-11-16 22:52:03 -0300

    Correções: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/hkprogrammer/cursomc/repositories/ItemPedidoRepository.java
+++ src/main/java/com/hkprogrammer/cursomc/repositories/ItemPedidoRepository.java
@@ -1,8 +1,6 @@
 package com.hkprogrammer.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hkprogrammer.cursomc.domain.Categoria;
 import com.hkprogrammer.cursomc.domain.ItemPedido;
 
 public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
