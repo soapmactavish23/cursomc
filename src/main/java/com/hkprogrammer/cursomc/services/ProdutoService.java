139013004b728d75fa5b184acd1445e720756d04
unknown <ricknogueira1231@gmail.com>
2022-11-18 16:52:25 -0300

    12:52

---
+++ src/main/java/com/hkprogrammer/cursomc/services/ProdutoService.java
@@ -0,0 +1,40 @@
+package com.hkprogrammer.cursomc.services;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.hkprogrammer.cursomc.domain.Categoria;
+import com.hkprogrammer.cursomc.domain.Produto;
+import com.hkprogrammer.cursomc.repositories.CategoriaRepository;
+import com.hkprogrammer.cursomc.repositories.ProdutoRepository;
+import com.hkprogrammer.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Produto find(Integer id) {
+		Produto produto = repo.findById(id).orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! " + id + ", Tipo: " + Produto.class.getName()));
+		
+		return produto;
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.search(nome, categorias, pageRequest);
+	}
+	
+}
